6f6112064c052152d51d69ce4a9c5221f6be35e2
annasnurdin <anasnurdin02@gmail.com>
2026-02-26 08:57:35 +0700

    create model example

--- database.sql
+++ database.sql
@@ -6,3 +6,15 @@ create table sample
 ) engine innodb;
 
 SELECT * from sample;
+
+CREATE TABLE customers (
+    id VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT null,
+    phone VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    constraint customer_email_unique UNIQUE (email),
+    constraint customer_email_phone UNIQUE (phone)
+) engine innodb;
+
+SELECT * FROM customers;
