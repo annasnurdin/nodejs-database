9ec8e992bd7fa6a8a14804cbf8e63b673e43ff17
annasnurdin <anasnurdin02@gmail.com>
2026-02-27 10:51:08 +0700

    create many

---
+++ tests/create-many.test.js
@@ -0,0 +1,25 @@
+import { prismaClient } from '../src/prisma-client'
+
+describe("Prisma Client", () => {
+    it('should can execute interactive transaction', async () => {
+      const { count } = await prismaClient.customer.createMany({
+        data: [
+            {
+                id: "annas",
+                email:"annas@aa.com",
+                phone: "123",
+                name: "Annas2"
+            },
+            {
+                id: "purwo",
+                email:"purwo@pp.com",
+                phone: "321",
+                name: "Purwo2"
+            }
+        ]
+      })
+
+       expect(count).toBe(2)
+    })
+    
+})
