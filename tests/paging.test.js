20a725308347ae9901ba1b817f872d69f1e49f09
annasnurdin <anasnurdin02@gmail.com>
2026-02-27 11:39:29 +0700

    paging

---
+++ tests/paging.test.js
@@ -0,0 +1,24 @@
+import { prismaClient } from '../src/prisma-client'
+
+describe("Prisma Client", () => {
+    it('should can do paging', async () => {
+      const page1 = await prismaClient.customer.findMany({
+        skip: 0,
+        take: 2,
+      })
+
+      console.info('page1:', page1)
+
+       expect(page1.length).toBe(2)
+
+       const page2 = await prismaClient.customer.findMany({
+        skip: 2,
+        take: 2,
+      })
+
+      console.info('page2:', page2)
+
+       expect(page2.length).toBe(2)
+    })
+    
+})
